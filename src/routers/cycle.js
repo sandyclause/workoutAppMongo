246502cd8c621c80496b2bacb2c9fbc5e8fbf50c
sandy <sandytian2000@hotmail.com>
2019-10-26 17:33:20 -0400

    delete and get cycles

--- src/routers/cycle.js
+++ src/routers/cycle.js
@@ -17,5 +17,32 @@ router.post('/cycles', auth, async (req, res) => {
   }
 })
 
+router.get('/cycles', auth, async (req, res) => {
+
+  try {
+    await req.user.populate({
+      path: 'cycles',
+    }).execPopulate()
+    res.send(req.user.cycles)
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
+router.delete('/cycles/:id', auth, async (req, res) => {
+  const cycleId = req.params.id;
+
+  try {
+    const cycle = await Cycle.findByIdAndDelete(cycleId);
+
+    if (!cycle) {
+      res.status(404).send()
+    }
+    res.status(201).send(cycle)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
 
 module.exports = router
